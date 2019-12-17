f5afcd6aa91e75ae2d4e9f66b1127d2f3cb57cd2
Emilio <emiliosanfilippo@gmail.com>
2019-12-17 17:41:16 +0100

    update in sparql generate query
    fixed issue with optional values; no problem as far as the XML is present although it is empty (e.g, <author></author>)

---
+++ SPARQL_Generate_mappigs/renumar_mapping.sh
@@ -0,0 +1,1 @@
+java -jar sparql-generate-jena.jar -d /data -q query/renumar_query.rqg -o data/out/out_renumar.ttl;
--- SPARQL_Generate_mappigs/renumar_mapping_v2.sh
+++
@@ -1,1 +0,0 @@
-java -jar sparql-generate-jena.jar -d /data -q query/renumar_query_v2.rqg -o data/out/out_renumar_v2.ttl;
